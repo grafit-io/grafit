6e021ae239f467b01089347bbd58114b0a61d844
Robin B <robin.buergi@gmail.com>
2018-10-25 14:33:40 +0200

    init drf backend

---
+++ backend/backend/urls.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.urls import path, include, re_path, reverse_lazy
+from rest_framework import routers
+from grafit import views
+from django.views.generic.base import RedirectView
+from rest_framework.authtoken.views import obtain_auth_token
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'users', views.UserCreateViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    # the 'api-root' from django rest-frameworks default router
+    # http://www.django-rest-framework.org/api-guide/routers/#defaultrouter
+    re_path(r'^$', RedirectView.as_view(url=reverse_lazy('api-root'), permanent=False)),
+]
---
+++ backend/grafit/views.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import Group
+from .models import User
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import AllowAny
+from grafit.serializers import UserSerializer, GroupSerializer, CreateUserSerializer
+
+
+class UserViewSet(mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  viewsets.GenericViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class UserCreateViewSet(mixins.CreateModelMixin,
+                        viewsets.GenericViewSet):
+    """
+    Creates user accounts
+    """
+    queryset = User.objects.all()
+    serializer_class = CreateUserSerializer
+    permission_classes = (AllowAny,)
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
