3a3917098efedb62aae9f385b16afdf16e283916
Robin B <robin.buergi@gmail.com>
2018-10-31 16:59:19 +0100

    fix(backend): moved static folder to /api/static/ for easier nginx mapping

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = []
-STATIC_URL = '/static/'
+STATIC_URL = '/api/static/'
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
