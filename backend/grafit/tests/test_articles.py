43db17b134b9ea8b0891967fae9c2268e7ab9911
Robin B <robin.buergi@gmail.com>
2018-11-14 15:53:19 +0100

    fix(backend): fix article_get test

--- backend/grafit/tests/test_articles.py
+++ backend/grafit/tests/test_articles.py
@@ -1,11 +1,13 @@
 from django.test import TestCase
 
-from ..models import Article
+from ..models import Article, Workspace
 
 
 class ArticleTest(TestCase):
     def setUp(self):
-        Article.objects.create(title="TestTitle", text="TestText")
+        workspace = Workspace(name="Testworkspace", initials="TE")
+        workspace.save()
+        Article.objects.create(title="TestTitle", text="TestText", workspace=workspace)
 
     def test_article_get(self):
         article = Article.objects.get(title="TestTitle")
