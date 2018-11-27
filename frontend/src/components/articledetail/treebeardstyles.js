grafit-io/grafit

commit f82dcd555f84030418eed1588a66551f677c87c6

feat(frontend): implement treeview for document surfing #22

frontend/src/components/articledetail/treebeardstyles.js
@@ -1,0 +1,69 @@
+export const TreeStyle = {
+  tree: {
+    base: {
+      listStyle: "none",
+      margin: 0,
+      padding: 0
+    },
+    node: {
+      link: {
+        cursor: "pointer",
+        position: "relative",
+        padding: "0px 5px",
+        display: "block"
+      },
+      activeLink: {
+        background: "#31363F"
+      },
+      toggle: {
+        base: {
+          position: "relative",
+          display: "inline-block",
+          verticalAlign: "top",
+          marginLeft: "-5px",
+          height: "24px",
+          width: "24px"
+        },
+        wrapper: {
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          margin: "-7px 0 0 -7px",
+          height: "14px"
+        },
+        height: 14,
+        width: 14,
+        arrow: {
+          fill: "#9DA5AB",
+          strokeWidth: 0
+        }
+      },
+      header: {
+        base: {
+          display: "inline-block",
+          verticalAlign: "top"
+        },
+        connector: {
+          width: "2px",
+          height: "12px",
+          borderLeft: "solid 2px black",
+          borderBottom: "solid 2px black",
+          position: "absolute",
+          top: "0px",
+          left: "-21px"
+        },
+        title: {
+          lineHeight: "24px",
+          verticalAlign: "middle"
+        }
+      },
+      subtree: {
+        listStyle: "none",
+        paddingLeft: "19px"
+      },
+      loading: {
+        color: "#E2C089"
+      }
+    }
+  }
+};

frontend/src/App.js
@@ -6,7 +6,7 @@ import {
   Switch
 } from "react-router-dom";
 import ArticleList from "./components/ArticleList";
-import ArticleDetail from "./components/ArticleDetail";
+import ArticleDetail from "./components/articledetail/ArticleDetail";
 import Login from "./components/login/Login";
 import Nagivation from "./components/navigation/Navigation";
 import Workspace from "./components/workspace/Workspace";

frontend/src/components/articledetail/ArticleDetail.jsx
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from "react";
 import { Link, Redirect } from "react-router-dom";
-import { APIService } from "../services/APIService";
+import { Treebeard } from "react-treebeard";
+import { APIService } from "../../services/APIService";
 import {
   Alert,
   FormGroup,
@@ -10,6 +11,7 @@ import {
   Button,
   ButtonToolbar
 } from "react-bootstrap";
+import { TreeStyle } from "./treebeardstyles";
 
 class ArticleDetail extends Component {
   state = {
@@ -21,12 +23,84 @@ class ArticleDetail extends Component {
     edit: false,
     new: false,
     alertSuccess: false,
-    redirectDeleted: false
+    redirectDeleted: false,
+    treebeardData: {}
+  };
+
+  constructor(props) {
+    super(props);
+    this.onToggle = this.onToggle.bind(this);
+  }
+
+  componentDidMount() {
+    if (this.props.location.state) {
+      const isNew = this.props.location.state.new;
+      if (isNew) {
+        this.setState({ new: true });
+      }
+    } else {
+      this.loadArticle(this.props.match.params.articleId);
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps) {
+      if (
+        nextProps.match.params.articleId !== this.props.match.params.articleId
+      ) {
+        this.loadArticle(nextProps.match.params.articleId);
+      }
+    }
+  }
+
+  getTreebeardData = () => {
+    return {
+      name: this.state.article.title,
+      toggled: true,
+      children: this.state.article.related.map(this.generateRelatedNode)
+    };
+  };
+
+  onToggle(node, toggled) {
+    console.log(node);
+    if (this.state.cursor) {
+      this.setState({
+        cursor: {
+          active: false
+        }
+      });
+    }
+    if (node.loading) {
+      console.log("load new data for: " + node.id);
+      APIService.getArticle(node.id)
+        .then(article => {
+          node.children = article.related.map(this.generateRelatedNode);
+          node.loading = false;
+        })
+        .then(() => {
+          node.active = true;
+          if (node.children) {
+            node.toggled = toggled;
+          }
+          this.setState({ cursor: node });
+        });
+    }
+  }
+
+  generateRelatedNode = related => {
+    console.log(related);
+    return {
+      id: related.id,
+      name: related.title,
+      loading: true,
+      children: []
+    };
   };
 
   loadArticle = articleId => {
     APIService.getArticle(articleId).then(article => {
       this.setState({ article: article });
+      this.setState({ treebeardData: this.getTreebeardData() });
     });
   };
 
@@ -79,27 +153,6 @@ class ArticleDetail extends Component {
       return "error";
     }
   };
-
-  componentDidMount() {
-    if (this.props.location.state) {
-      const isNew = this.props.location.state.new;
-      if (isNew) {
-        this.setState({ new: true });
-      }
-    } else {
-      this.loadArticle(this.props.match.params.articleId);
-    }
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps) {
-      if (
-        nextProps.match.params.articleId !== this.props.match.params.articleId
-      ) {
-        this.loadArticle(nextProps.match.params.articleId);
-      }
-    }
-  }
 
   render() {
     if (this.state.redirectDeleted) {
@@ -242,6 +295,12 @@ class ArticleDetail extends Component {
                 </Link>
               ))}
               <p>{this.state.article.text}</p>
+              <h3>Related Articles</h3>
+              <Treebeard
+                data={this.state.treebeardData}
+                onToggle={this.onToggle}
+                style={TreeStyle}
+              />
             </div>
           )}
         </div>

commit 4d065be0792f4d83a4085904d94fd03470fa995c

fix(frontend): resolve treebeard style issue with firefox #22 Regression bug from commit f82dcd555f84030418eed1588a66551f677c87c6 Fix: https://github.com/storybooks/react-treebeard/issues/148

frontend/src/components/articledetail/treebeardstyles.js
@@ -6,6 +6,17 @@ export const TreeStyle = {
       padding: 0
     },
     node: {
+      container: {
+        link: {
+          cursor: "pointer",
+          position: "relative",
+          padding: "0px 5px",
+          display: "block"
+        },
+        activeLink: {
+          background: "#31363F"
+        }
+      },
       link: {
         cursor: "pointer",
         position: "relative",