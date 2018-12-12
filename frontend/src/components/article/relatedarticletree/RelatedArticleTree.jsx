cda90b6e1a1ad627cf34d7574825e0824b242e7c
Robin B <robin.buergi@gmail.com>
2018-12-12 16:56:45 +0100

    feat(frontend): filter out cyclic relations in concept surfing tree #31

--- frontend/src/components/article/relatedarticletree/RelatedArticleTree.jsx
+++ frontend/src/components/article/relatedarticletree/RelatedArticleTree.jsx
@@ -9,15 +9,15 @@ export default class RelatedArticleTree extends Component {
     treebeardData: undefined
   };
 
-  componentDidMount() {
+  async componentDidMount() {
     if (this.props.article && this.props.article.id) {
       this.setState({
-        treebeardData: this.getTreebeardData(this.props.article)
+        treebeardData: await this.getTreebeardData(this.props.article)
       });
     }
   }
 
-  componentWillReceiveProps(nextProps) {
+  async componentWillReceiveProps(nextProps) {
     if (
       (nextProps &&
         nextProps.article &&
@@ -26,19 +26,29 @@ export default class RelatedArticleTree extends Component {
       nextProps.article.related !== this.props.article.related
     ) {
       this.setState({
-        treebeardData: this.getTreebeardData(nextProps.article)
+        treebeardData: await this.getTreebeardData(nextProps.article)
       });
     }
   }
 
-  getTreebeardData = article => {
+  getTreebeardData = async article => {
+    let children = await Promise.all(
+      article.related.map(async related => {
+        let children = [];
+        let relatedArticle = await APIService.getArticle(related.id);
+        if (relatedArticle.related.length <= 1) {
+          children = undefined;
+        }
+        return this.generateRelatedNode(related, [article.id], 1, children);
+      })
+    );
+
     return {
+      parent: undefined,
       id: article.id,
       name: article.title,
       toggled: true,
-      children: article.related.map(related =>
-        this.generateRelatedNode(related, 1)
-      ),
+      children: children,
       level: 0
     };
   };
@@ -54,9 +64,18 @@ export default class RelatedArticleTree extends Component {
     if (node.loading) {
       APIService.getArticle(node.id)
         .then(article => {
-          node.children = article.related.map(related =>
-            this.generateRelatedNode(related, node.level + 1)
-          );
+          node.children = article.related.map(related => {
+            let children = [];
+            if (node.parents.includes(related.id)) {
+              children = undefined;
+            }
+            return this.generateRelatedNode(
+              related,
+              [...node.parents, article.id],
+              node.level + 1,
+              children
+            );
+          });
           node.loading = false;
         })
         .then(() => {
@@ -73,14 +92,16 @@ export default class RelatedArticleTree extends Component {
     }
   };
 
-  generateRelatedNode = (related, level) => {
-    if (level > 5) {
+  generateRelatedNode = (related, parents, level, children) => {
+    if (!children || level > 5) {
       return {
+        parents: parents,
         id: related.id,
         name: related.title
       };
     }
     return {
+      parents: parents,
       id: related.id,
       name: related.title,
       loading: true,
